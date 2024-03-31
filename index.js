877bd8c1fb9d6ea8eec4dbef5290e4d539313a5f
sauravsitoula <saaurav10@gmail.com>
2024-03-31 15:40:58 -0400

    testng

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  console.log("something");
+});
 
 app.use("/auth", authRoutes);
 app.use(validateToken);
